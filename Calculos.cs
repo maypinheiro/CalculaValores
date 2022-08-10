55388c70da37133e305e9ea731b17247e86d15a4
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-10 09:52:16 -0300

    Calculadora

--- Calculos.cs
+++ Calculos.cs
@@ -11,23 +11,23 @@ namespace CalculadoraDeValores
         {
                 float imc = peso / (altura * altura);
 
-                if (imc < 18.5) return $"IMC: { imc } A baixo do peso.";
-                else if (imc < 24.9) return $"IMC: {imc} Peso Normal.";
-                else if (imc < 29.9) return $"IMC: {imc} Excesso de peso.";
-                else if (imc < 35) return $"IMC: {imc} Obesidade.";
+                if (imc < 18.5) return $"IMC: { imc.ToString("N2") } A baixo do peso.";
+                else if (imc < 24.9) return $"IMC: {imc.ToString("N2")} Peso Normal.";
+                else if (imc < 29.9) return $"IMC: {imc.ToString("N2")} Excesso de peso.";
+                else if (imc < 35) return $"IMC: {imc.ToString("N2")} Obesidade.";
                 else return $"IMC: {imc} Obesidade extrema.";
         }
         public  String CalculaArea( double raioDoCirculo)
         {
                  double areaCirculo = Math.PI * Math.Pow(raioDoCirculo, 2);
 
-                 return $"Área do circulo: { areaCirculo} N2"; 
+                 return $"Área do circulo: {areaCirculo.ToString("N2")}"; 
         }
         public  String CalculaRegraDeTres(float a, float b, float c)
         {
                 float d = (c * b) / a;
 
-                return $" Valor de X: { d }";
+                return $" Valor de X: {d}";
         }
 
     }
